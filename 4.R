867a5af4d4cb34ff6a7f139163111b6fc09973f9
zzungokong <57696730+zzungokong@users.noreply.github.com>
2022-04-10 13:27:10 +0700

    lah gampang

---
+++ 4.R
@@ -0,0 +1,14 @@
+#4a
+dof = 10
+x = 2
+dchisq(2, 10)
+
+#4b
+dof = 10
+x = 100
+y = rchisq(x, dof)
+hist(y)
+\
+#4c
+#Mean dari Distribusi chisquare adalah v
+#Varians dari Distribusi chisquare adalah 2v
