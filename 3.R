0929f793b8f8b10f94edfd37daf22a04f9963d26
zzungokong <57696730+zzungokong@users.noreply.github.com>
2022-04-10 13:11:28 +0700

    nomer 3 ez

---
+++ 3.R
@@ -0,0 +1,18 @@
+#3a
+lambda = 4.5
+x = 6
+dpois(x, lambda)
+
+#3b
+set.seed(0)
+n = 365
+y = rpois(n, lambda)
+hist(y)
+z = (rpois(n, lambda) == 6)
+mean(z)
+
+#3c
+#Tidak terpaut jauh
+
+#3d
+#Mean dan varians Distribusi Poison adalah lambda * t
