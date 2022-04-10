56020efdd85316a92824aa00e39407abae519dd9
zzungokong <57696730+zzungokong@users.noreply.github.com>
2022-04-10 19:24:32 +0700

    no 5 done

---
+++ 5.R
@@ -0,0 +1,27 @@
+#5a
+#fungsinya adalah 1/betha e^(-x/betha)
+
+#5b
+x = 10
+b = 5
+y = rexp(x, 5)
+hist(y)
+
+x = 100
+b = 5
+y = rexp(x, 5)
+hist(y)
+
+x = 1000
+b = 5
+y = rexp(x, 5)
+hist(y)
+
+x = 10000
+b = 5
+y = rexp(x, 5)
+hist(y)
+
+#5c
+#Mean dari Distribusi Eksponen adalah betha
+#Varians dari Distribusi Eksponen adalah betha^2
